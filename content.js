6f9d19fc5e0edd1bafc9f1c1f18a8e55e14d471c
Bohdan Hryhorovych <bohdan.hryhorovych@gmail.com>
2023-06-25 10:26:19 +0200

    First commit

---
+++ content.js
@@ -0,0 +1,14 @@
+function modifyDOM() {
+  let videos = document.querySelectorAll('ytd-rich-item-renderer.ytd-rich-grid-row');
+  const VIDEOS_TO_SHOW = 3;
+
+  for (let i = 0; i < videos.length; i++) {
+    if (i >= VIDEOS_TO_SHOW) {
+      videos[i].style.display = 'none';
+    }
+  }
+}
+
+let observer = new MutationObserver(modifyDOM);
+
+observer.observe(document.body, {childList: true, subtree: true});
